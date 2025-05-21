c25c1b47859d7415a6ab4f1d08631d693b8f72a6
CODERAKS11 <kk9546x@gmail.com>
2025-05-21 15:55:59 +0530

    Seller Signup Form

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css'
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+  isLoginMode = true;
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // Initialization logic if needed
+  }
+
+  signup(data: object): void {
+    console.warn('Signing up with', data);
+  }
 
 }
