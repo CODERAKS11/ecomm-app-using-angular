14a5afe42501b5d5a98c1018e1669ad5a6ef584d
CODERAKS11 <kk9546x@gmail.com>
2025-05-22 00:13:05 +0530

    Navbar for Seller

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuType: string = 'default';
+  constructor(private route: Router) { }
+
+  ngOnInit(): void {
+    
+    this.route.events.subscribe((val: any) => {
+      console.warn(val.url);//logging the route change events
+      if(val.url){
+        if(localStorage.getItem('seller') && (val.url.includes('seller-auth') || val.url.includes('seller'))) {
+          this.menuType = 'seller';
+        } else{
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 
 }
