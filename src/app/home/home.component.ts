cadf2a205ba8f075b773db1951e7bde1b99316c5
CODERAKS11 <kk9546x@gmail.com>
2025-05-23 11:29:22 +0530

    trendy products display at home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,16 @@ import { product } from '../data-type';
 })
 export class HomeComponent implements OnInit {
   popularProducts: undefined | product[];
+  trendyProducts: undefined | product[];
   constructor(private product : ProductService) {}
   ngOnInit(): void {
     this.product.popularProducts().subscribe((data) => {
       console.warn(data);
       this.popularProducts = data;
+    });
+    this.product.trendyProducts().subscribe((data) => {
+      console.warn(data);
+      this.trendyProducts = data;
     });
   }
 
