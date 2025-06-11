eaaf5f1ef5fb3f6e6580c0bb439b31366211e3fa
CODERAKS11 <kk9546x@gmail.com>
2025-06-11 22:29:00 +0530

    my orders page ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { UserAuthComponent } from './user-auth/user-auth.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,7 +23,8 @@ const routes: Routes = [
   { path: 'details/:productId',component: ProductDetailsComponent},
   { path: 'user-auth', component: UserAuthComponent },
   { path: 'cart-page', component: CartPageComponent},
-  { path: 'checkout', component: CheckoutComponent}
+  { path: 'checkout', component: CheckoutComponent},
+  { path: 'my-orders', component:MyOrdersComponent}
 ];
 
 @NgModule({
